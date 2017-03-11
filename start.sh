76a395b22d90f7056593a15dd3d6de01b52529b4
Esteban Ordano <eordano@gmail.com>
2017-03-11 15:00:58 -0800

    Update "start" script

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 # seed node
-./bin/decentraland-node --fast --port=2001 --prefix="/home/maraoz/.d1" --httpport=8001 --n=regtest --apikey=38Dpwnjsj2zn3QETJ6GKv8YkHomA
+./bin/decentraland-node --fast --port=2001 --prefix="~/.d1" --httpport=8001 --n=testnet --apikey=38Dpwnjsj2zn3QETJ6GKv8YkHomA --contentPort=9301
 
 
 # peer 2
-./bin/decentraland-node --fast --port=2002 --seed=localhost:2001 --prefix="/home/maraoz/.d2" --httpport=8002 --n=regtest --apikey=38Dpwnjsj2zn3QETJ6GKv8YkHomA
+./bin/decentraland-node --fast --port=2002 --seed=localhost:2001 --prefix="~/.d2" --httpport=8002 --n=testnet --apikey=38Dpwnjsj2zn3QETJ6GKv8YkHomA --contentPort=9302
 
 # peer 3
-./bin/decentraland-node --fast --port=2003 --seed=localhost:2001 --prefix="/home/maraoz/.d3" --httpport=8003 --n=regtest --apikey=38Dpwnjsj2zn3QETJ6GKv8YkHomA
+./bin/decentraland-node --fast --port=2003 --seed=localhost:2001 --prefix="`/.d3" --httpport=8003 --n=testnet --apikey=38Dpwnjsj2zn3QETJ6GKv8YkHomA --contentPort=9303
 
