13c7aa84a67e19401baec78b3ced2bd3131738e9
Esteban Ordano <eordano@gmail.com>
2017-03-16 08:57:13 -0700

    Add script to plot controlled tiles

---
+++ scripts/plot-controlled.js
@@ -0,0 +1,39 @@
+var fs = require('fs')
+var randomcolor = require('randomcolor')
+
+var c = fs.readFileSync('/dev/stdin').toString()
+var f = JSON.parse(c)
+
+var colormap = {}
+
+f.sort(function(a, b) {
+  return Math.abs(a.x) + Math.abs(a.y) - (Math.abs(b.x) + Math.abs(b.y))
+})
+
+console.log(`
+  <html>
+  <head>
+  <style>
+    html, head, body {
+      margin: 0;
+      padding: 0;
+      display: block;
+    }
+    .tiles { position: absolute; display: block; width: 10px; height: 10px; }
+    .canvas { position: absolute; top: 500px; left: 500px; width: 100%; height: 100%; }
+  </style>
+  </head>
+  <body>
+  <div class="canvas">
+`)
+for (let i in f) {
+  const y = f[i]
+  const key = y.controlled ? 1 : 0 
+  const color = key && '#ff0000' || '#333333'
+  console.log(`<div class="tiles" style="left: ${y.x * 10}px; top: ${y.y * 10}px; background-color: ${color}"></div>`);
+}
+console.log(`
+  </div>
+  </body>
+  </html>
+`)
