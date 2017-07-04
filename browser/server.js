fb350c82eaa2feb1b204a8d079c1cfa7a1d94a4e
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-07-04 21:01:07 +0200

    WEB-1 First version of the node web interface

--- browser/server.js
+++ browser/server.js
@@ -1,24 +1,15 @@
 'use strict';
 
 var HTTPBase = require('../lib/http/base');
-var WSProxy = require('./wsproxy');
+var co = require('../lib/utils/co');
+var Client = require('../lib/http/client');
 var fs = require('fs');
 
 var server = new HTTPBase();
-var proxy = new WSProxy({
-  pow: process.argv.indexOf('--pow') !== -1,
-  ports: [8333, 18333, 18444, 28333, 28901]
-});
-
-proxy.on('error', function(err) {
-  console.error(err.stack + '');
-});
-
+var port = +process.argv[2] || 8080
 var index = fs.readFileSync(__dirname + '/index.html');
-var indexjs = fs.readFileSync(__dirname + '/index.js');
-var decentraland = fs.readFileSync(__dirname + '/decentraland.js');
-var master = fs.readFileSync(__dirname + '/decentraland-master.js');
-var worker = fs.readFileSync(__dirname + '/decentraland-worker.js');
+var indexjs = fs.readFileSync(__dirname + '/js/index.js');
+var maincss = fs.readFileSync(__dirname + '/css/main.css');
 
 server.get('/favicon.ico', function(req, res, send, next) {
   send(404, '', 'text');
@@ -27,27 +18,46 @@ server.get('/favicon.ico', function(req, res, send, next) {
 server.get('/', function(req, res, send, next) {
   send(200, index, 'html');
 });
-
-server.get('/index.js', function(req, res, send, next) {
+server.get('/js/index.js', function(req, res, send, next) {
   send(200, indexjs, 'js');
 });
-
-server.get('/decentraland.js', function(req, res, send, next) {
-  send(200, decentraland, 'js');
+server.get('/css/main.css', function(req, res, send, next) {
+  send(200, maincss, 'css');
 });
 
-server.get('/decentraland-master.js', function(req, res, send, next) {
-  send(200, master, 'js');
-});
-
-server.get('/decentraland-worker.js', function(req, res, send, next) {
-  send(200, worker, 'js');
+server.get('/css/main.css', function(req, res, send, next) {
+  send(200, maincss, 'css');
 });
 
 server.on('error', function(err) {
   console.error(err.stack + '');
 });
 
-proxy.attach(server.server);
+console.log('Running server on port ' + port)
+server.listen(port);
 
-server.listen(+process.argv[2] || 8080);
+
+function API(apiKey) {
+  this.config = {
+    apiKey: apiKey
+  };
+}
+
+API.prototype.rpc = co(function* rpc(method, params) {
+  var client = new Client({
+    apiKey: this.config.apiKey
+  });
+
+  let result = yield client.rpc.call(method, params);
+  result = JSON.parse(result);
+  result.sort((a, b) => a.x + b.x);
+
+  console.log('Controlled tiles:');
+  for (let i in result) {
+    const y = result[i];
+    console.log(y.x, y.y);
+  }
+});
+
+// const api = new API('dland-key-256');
+// api.rpc('dumpblockchain', [ true ]);
